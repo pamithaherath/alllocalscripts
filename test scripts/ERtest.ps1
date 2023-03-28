92f2b5693db50d42e92bd123012f61aaf9ccb751
Pamitha Herath <pherathmudiy@dxc.com>
2023-03-28 18:53:59 +1100

    new commit to new branch

---
+++ test scripts/ERtest.ps1
@@ -0,0 +1,13 @@
+$initiatingCircuitName = "erc-ninp-conn-pnp-erc-aue1"
+$rg = "rg-ninp-conn-expressroute-pnp-aue1"
+$circuitConnectionName = "cxn-vgw-npp-con-erc-aue-001"
+$conRG = "rg-npp-con-networkhub-aue"
+
+#Remove-AzVirtualNetworkGatewayConnection -Name $circuitConnectionName -ResourceGroupName $conRG -Force
+
+
+#$circuit_init = Get-AzExpressRouteCircuit -Name $initiatingCircuitName -ResourceGroupName $rg
+
+#Remove-AzExpressRouteCircuitConnectionConfig -Name $circuitConnectionName -ExpressRouteCircuit $circuit_init
+#Set-AzExpressRouteCircuit -ExpressRouteCircuit $circuit_init
+
